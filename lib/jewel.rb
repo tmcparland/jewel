d9d777ab5313eb987405d6a21d25b980523fe717
Tara McParland <tara.mcparland@gmail.com>
2017-01-07 05:04:17 +0000

    create message with error

--- lib/jewel.rb
+++ lib/jewel.rb
@@ -32,7 +32,22 @@ class Jewel
   
   def get_messages (message_page=1)
     response = self.class.get(storage("message_threads?page=#{message_page}"), headers: {"authorization" => @auth_token})
-    @checkpoint_data = JSON.parse(response.body)
+    @message_data = JSON.parse(response.body)
+  end
+
+  def create_message (sender, recipient_id, subject, message)
+    response = self.class.post(storage("messages/"), body: {"sender": sender_email, "recipient_id": recipient_id, "subject": subject, "stripped-text": message}, headers: {"authorization" => @auth_token })
+    @create_message_data = JSON.parse(response.body)
+  end 
+  
+  def create_submission (enrollment_id, checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    response = self.class.post(storage("checkpoints_submissions/"), body: {"checkpoint_id": checkpoint_id, "enrollment_id": enrollment_id, "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "comment": comment} headers: {"authorization" => @auth_token})
+    @submission_data = JSON.parse(response.body)
+  end
+
+  def update_submission (id)
+    response = self.class.get(storage("checkpoints_submissions/:#{id}"), headers: {"authorization" => @auth_token})
+    @update_data = JSON.parse(response.body)
   end
   
   
